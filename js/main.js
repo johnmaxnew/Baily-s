9928e8794d3834a389ffe93de8191f73ca432b7f
johnmaxnew <forevermeticulous@gmail.com>
2021-10-30 23:43:16 +0300

    Modified markup

--- js/main.js
+++ js/main.js
@@ -9,3 +9,9 @@ function toggleNav() {
     navToggler.classList.toggle('active')
     document.querySelector('.nav').classList.toggle('open');
 }
+
+document.addEventListener('click', function(e){
+    if(e.target.closest('.nav-item')){
+        toggleNav();
+    }
+})
